90177a604a4d3c12452bcc8f9a278f28fa688061
Khoa Tran <>
2025-09-30 10:41:59 +0700

    Init project

---
+++ src/test/java/mk/gameservice/expedition/ExpeditionApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.gameservice.expedition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpeditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
